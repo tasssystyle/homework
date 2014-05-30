e60bfc383fb2bbfb56a840a7b4ef5f1d9da9fb7e
tasssystyle <tasssystyle@yhoo.co.jp>
2014-05-31 05:25:05 +0900

    standard

---
+++ standard.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+        return 0;
+     }
